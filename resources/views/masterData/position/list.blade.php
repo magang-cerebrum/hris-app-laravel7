65792cc816b921de24235d63388c5cd3120dbafe
mesayusriana12 <mesayusriana12@gmail.com>
2021-02-05 16:23:15 +0700

    update crud divisi,jabatan,shift

--- resources/views/masterData/position/list.blade.php
+++ resources/views/masterData/position/list.blade.php
@@ -1,104 +1,93 @@
-@extends('layout/template')
+@extends('layouts/templateAdmin')
+@section('content-title','Data Jabatan')
+@section('content-subtitle','HRIS PT. Cerebrum Edukanesia Nusantara')
 @section('title','Masterdata Jabatan')
 @section('content')
-<div class="boxed">
-    <!--CONTENT CONTAINER-->
-    <!--===================================================-->
 
-    <div id="content-container">
-        <div id="page-head">
-
-            <div class="pad-all text-center">
-                <h3>Data Jabatan</h3>
-            </div>
-        </div>
-
-
-        <!--Page content-->
-        <!--===================================================-->
-        <div id="page-content">
-
-            <div class="panel">
-                <div class="panel-heading">
-                    <h3 class="panel-title">List Jabatan</h3>
+<div class="panel">
+    <div class="panel-heading">
+        <h3 class="panel-title text-bold text-center">Data Jabatan</h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-sm-12">
+                @if (session('status'))
+                <div class="alert alert-info alert-dismissable">
+                    <button class="close" data-dismiss="alert"><i class="pci-cross pci-circle"></i></button>
+                    {{session('status')}}
                 </div>
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            @if (session('status'))
-                                <div class="alert alert-info">
-                                    {{session('status')}}
-                                </div>
-                            @endif
-                            <div class="row">
-                                <div class="col-sm-2">
-                                    <a href="{{url('/data/position/add')}}" class="btn btn-primary btn-labeled"
-                                        style="margin-bottom:15px">
-                                        <i class="btn-label fa fa-plus"></i>
-                                        Tambah Jabatan
-                                    </a>
-                                </div>
-                                <div class="col-sm-7"></div>
-                                <div class="col-sm-3">
-                                    <div class="form-group float-right">
-                                        <input type="text" name="cari-jabatan" id="cari-jabatan" class="form-control" placeholder="Cari Jabatan"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <table id="masterdata-position"
-                                class="table table-striped table-bordered dataTable no-footer dtr-inline collapsed"
-                                role="grid" aria-describedby="demo-dt-basic_info" style="width: 100%;" width="100%"
-                                cellspacing="0">
-                                <thead>
-                                    <tr role="row">
-                                        <th class="sorting_asc text-center" tabindex="0" aria-controls="demo-dt-basic" rowspan="1"
-                                            colspan="1" aria-sort="ascending"
-                                            aria-label="Name: activate to sort column descending">ID</th>
-                                        <th class="sorting text-center" tabindex="0" aria-controls="demo-dt-basic" rowspan="1"
-                                            colspan="1" 
-                                            aria-label="Position: activate to sort column ascending">Name</th>
-                                        <th class="sorting text-center" tabindex="0" aria-controls="demo-dt-basic" rowspan="1"
-                                            colspan="1" 
-                                            aria-label="Position: activate to sort column ascending">Action</th>    
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($position as $row)
-                                    <tr>
-                                        <td tabindex="0" class="sorting_1 text-center">{{$row->id}}</td>
-                                        <td class="text-center">{{$row->name}}</td>
-                                        <td class="text-center">
-                                            <a href="/data/position/{{$row->id}}/edit" class="btn btn-success btn-icon btn-circle add-tooltip" data-toggle="tooltip" data-container="body" data-placement="top" data-original-title="Edit Jabatan" type="button">
-                                                <i class="ion-edit icon-lg"></i>
-                                            </a>
-                                            <form action="/data/position/{{$row->id}}" method="POST" style="display: inline; margin: auto 5px">
-                                                @method('delete')
-                                                @csrf
-                                                <button class="btn btn-pink btn-icon btn-circle add-tooltip" data-toggle="tooltip" data-container="body" data-placement="top" data-original-title="Hapus Jabatan">
-                                                    <i class="ion-trash-b icon-lg"></i>
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            <div class="row">
-                                <div class="col-sm-5"></div>
-                                <div class="col-sm-2">
-                                    <ul class="pagination">
-                                        {{ $position->links() }}
-                                    </ul>
-                                </div>
-                                <div class="col-sm-5"></div>
-                            </div>
+                @endif
+                <div class="row">
+                    <div class="col-sm-2">
+                        <a href="{{url('/admin/position/add')}}" class="btn btn-primary btn-labeled"
+                            style="margin-bottom:15px">
+                            <i class="btn-label fa fa-plus"></i>
+                            Tambah Jabatan
+                        </a>
+                    </div>
+                    <div class="col-sm-7"></div>
+                    <div class="col-sm-3 hidden">
+                        <div class="form-group float-right">
+                            <input type="text" name="cari-jabatan" id="cari-jabatan" class="form-control"
+                                placeholder="Cari Jabatan" />
                         </div>
                     </div>
+                </div>
+                <table id="masterdata-position"
+                    class="table table-striped table-bordered dataTable no-footer dtr-inline collapsed" role="grid"
+                    aria-describedby="demo-dt-basic_info" style="width: 100%;" width="100%" cellspacing="0">
+                    <thead>
+                        <tr role="row">
+                            <th class="sorting_asc text-center" tabindex="0" aria-controls="demo-dt-basic" rowspan="1"
+                                colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending">
+                                ID Jabatan</th>
+                            <th class="sorting text-center" tabindex="0" aria-controls="demo-dt-basic" rowspan="1"
+                                colspan="1" aria-label="Position: activate to sort column ascending">Nama Jabatan</th>
+                            <th class="sorting text-center" tabindex="0" aria-controls="demo-dt-basic" rowspan="1"
+                                colspan="1" aria-label="Position: activate to sort column ascending">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($position as $row)
+                        <tr>
+                            <td tabindex="0" class="sorting_1 text-center">{{$row->id}}</td>
+                            <td class="text-center">{{$row->name}}</td>
+                            <td class="text-center">
+                                <a href="/admin/position/{{$row->id}}/edit"
+                                    class="btn btn-success btn-icon btn-circle add-tooltip" data-toggle="tooltip"
+                                    data-container="body" data-placement="top" data-original-title="Edit Jabatan"
+                                    type="button">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                <form action="/admin/position/{{$row->id}}" method="POST"
+                                    style="display: inline; margin: auto 5px">
+                                    @method('delete')
+                                    @csrf
+                                    <button class="btn btn-pink btn-icon btn-circle add-tooltip" data-toggle="tooltip"
+                                        data-container="body" data-placement="top" data-original-title="Hapus Jabatan">
+                                        <i class="fa fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="row">
+                    <div class="col-sm-5"></div>
+                    <div class="col-sm-2">
+                        <ul class="pagination">
+                            {{ $position->links() }}
+                        </ul>
+                    </div>
+                    <div class="col-sm-5"></div>
                 </div>
             </div>
         </div>
     </div>
-    {{-- <script>
+</div>
+
+{{-- <script>
         $(document).ready(function(){
 
             fetch_data();
@@ -106,20 +95,20 @@
             function fetch_data(query =''){
                 $.ajax({
                     url:"{{ route('data.division.search') }}",
-                    method:'GET',
-                    data:{query:query},
-                    dataType:'json',
-                    success:function(data)
-                    {
-                        ('tbody').html(data.table_data);
-                    }
-                })
-            }
+method:'GET',
+data:{query:query},
+dataType:'json',
+success:function(data)
+{
+('tbody').html(data.table_data);
+}
+})
+}
 
-            $(document).on('keyup','#cari-divisi',function(){
-                var query = $(this).val();
-                fetch_data(query);
-            });
-        });
-    </script> --}}
-    @endsection
+$(document).on('keyup','#cari-divisi',function(){
+var query = $(this).val();
+fetch_data(query);
+});
+});
+</script> --}}
+@endsection
--- resources/views/masterData/shift/edit.blade.php
+++ resources/views/masterData/shift/edit.blade.php
@@ -1,92 +1,78 @@
-@extends('layout/template')
+@extends('layouts/templateAdmin')
+@section('content-title','Edit Data Shift')
+@section('content-subtitle','HRIS PT. Cerebrum Edukanesia Nusantara')
 @section('title','Edit Shift')
 @section('content')
 <!--Bootstrap Timepicker [ OPTIONAL ]-->
-<link href="{{asset("admin/demo/plugins/bootstrap-timepicker/bootstrap-timepicker.min.css")}}" rel="stylesheet">
-<div class="boxed">
-    <!--CONTENT CONTAINER-->
-    <!--===================================================-->
+<link href="{{asset("plugins/bootstrap-timepicker/bootstrap-timepicker.min.css")}}" rel="stylesheet">
 
-    <div id="content-container">
-        <div id="page-head">
-
-            <div class="pad-all text-center">
-                <h3>Edit Shift</h3>
-            </div>
-        </div>
-
-
-        <!--Page content-->
-        <!--===================================================-->
-        <div id="page-content">
-
-            <div class="panel">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Form Edit Shift</h3>
+<div class="panel">
+    <div class="panel-heading">
+        <h3 class="panel-title text-bold text-center">Form Edit Shift</h3>
+    </div>
+    <div class="panel-body">
+        <form class="form-horizontal" action="/admin/shift/{{$shift->id}}" method="POST">
+            @csrf
+            @method('put')
+            <div class="panel-body">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label" for="hor-editshift">Nama Shift
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" placeholder="Nama Shift" name="name"
+                            class="form-control @error('name') is-invalid @enderror" value="{{$shift->name}}">
+                        @error('name') <div class="text-danger invalid-feedback mt-3">
+                            Nama shift tidak boleh kosong.
+                        </div> @enderror
+                    </div>
                 </div>
-                <div class="panel-body">
-                    <form class="form-horizontal" action="/data/shift/{{$shift->id}}" method="POST">
-                        @csrf
-                        @method('put')
-                        <div class="panel-body">
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label" for="hor-editshift">Nama Shift
-                                </label>
-                                <div class="col-sm-10">
-                                    <input type="text" placeholder="Nama Shift" name="name"
-                                        class="form-control @error('name') is-invalid @enderror"
-                                        value="{{$shift->name}}">
-                                    @error('name') <div class="text-danger invalid-feedback mt-3">
-                                        Nama shift tidak boleh kosong.
-                                    </div> @enderror
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label">Jam Masuk:</label>
-                                    <div class="col-sm-4">
-                                        <div class="input-group date">
-                                            <input id="timepicker-edit-shift-masuk" type="text" class="form-control" name="start_working_time" value="{{$shift->start_working_time}}">
-                                            <span class="input-group-addon"><i class="ti-timer"></i></span>
-                                        </div>
-                                    </div>
-                                    <label class="col-sm-2 control-label">Jam Keluar:</label>
-                                    <div class="col-sm-4">
-                                        <div class="input-group date">
-                                            <input id="timepicker-edit-shift-keluar" type="text" class="form-control" name="end_working_time" value="{{$shift->end_working_time}}">
-                                            <span class="input-group-addon"><i class="ti-timer"></i></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel-footer text-right">
-                                <button class="btn btn-mint" type="submit">Simpan</button>
+                <div class="form-group">
+                    <div class="row">
+                        <label class="col-sm-2 control-label">Jam Masuk:</label>
+                        <div class="col-sm-4">
+                            <div class="input-group date">
+                                <input id="timepicker-edit-shift-masuk" type="text" class="form-control"
+                                    name="start_working_time" value="{{$shift->start_working_time}}">
+                                <span class="input-group-addon"><i class="ti-timer"></i></span>
                             </div>
                         </div>
-                    </form>
+                        <label class="col-sm-2 control-label">Jam Keluar:</label>
+                        <div class="col-sm-4">
+                            <div class="input-group date">
+                                <input id="timepicker-edit-shift-keluar" type="text" class="form-control"
+                                    name="end_working_time" value="{{$shift->end_working_time}}">
+                                <span class="input-group-addon"><i class="ti-timer"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer text-right">
+                    <button class="btn btn-mint" type="submit">Simpan</button>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
-    <!--Bootstrap Timepicker [ OPTIONAL ]-->
-    <script src="{{asset("admin/demo/plugins/bootstrap-timepicker/bootstrap-timepicker.min.js")}}"></script>
+</div>
 
-    <script>
-        $(document).ready(function () {
-            $('#timepicker-edit-shift-masuk').timepicker({
-                showSeconds: true,
-                secondStep: 15,
-                showMeridian: false
-            });
+<!--Bootstrap Timepicker [ OPTIONAL ]-->
+<script src="{{asset("plugins/bootstrap-timepicker/bootstrap-timepicker.min.js")}}"></script>
+
+<script>
+    $(document).ready(function () {
+        $('#timepicker-edit-shift-masuk').timepicker({
+            showSeconds: true,
+            secondStep: 15,
+            showMeridian: false
         });
+    });
 
-        $(document).ready(function () {
-            $('#timepicker-edit-shift-keluar').timepicker({
-                showSeconds: true,
-                secondStep: 15,
-                showMeridian: false
-            });
+    $(document).ready(function () {
+        $('#timepicker-edit-shift-keluar').timepicker({
+            showSeconds: true,
+            secondStep: 15,
+            showMeridian: false
         });
+    });
 
-    </script>
-    @endsection
+</script>
+@endsection

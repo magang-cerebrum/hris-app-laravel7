3589dcb96912aca5f83b54fe1c69735b5af5e51f
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-02-08 17:03:17 +0700

    edit controller admin dashboard

--- app/Http/Controllers/AdminAuthDashboardController.php
+++ app/Http/Controllers/AdminAuthDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\MasterUser;
+use App\MasterRecruitment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;

1c921603febeadb5f2a3f4e30f9367d2f0f3d258
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-02-09 16:51:42 +0700

    recruitment + job

--- resources/views/masterData/job/job.blade.php
+++ resources/views/masterData/job/job.blade.php
@@ -9,57 +9,77 @@
             <div class="panel-body">
                 <div class="table-responsive">
                     <div class="row">
-                        <div class="col-sm-2">
+                        <div class="col-sm-6 jobTambah">
                             <a href="{{url('/admin/job/add')}}" class="btn btn-primary btn-labeled"
                                 style="margin-bottom:15px">
                                 <i class="btn-label fa fa-plus"></i>
                                 Tambah Divisi
                             </a>
-                        </div>
-                    </div>
                     @if (count($dataJob) == 0)
+                        </div>
+                        </div>
                         <div class="text-center">
                             <h1 class="h3">Data Kosong / Data Tidak Ditemukan</h1>
                             <img src="{{ asset('img/title-cerebrum.png')}}" style="width: 250px">
                         </div>
                         @else
-                            <table id="masterdata-division"
-                            class="table table-striped table-bordered dataTable no-footer dtr-inline collapsed"
-                            role="grid" aria-describedby="demo-dt-basic_info" style="width: 100%;" width="100%"
-                            cellspacing="0">
-                                <thead>
-                                    <tr role="row">
-                                        <th class="sorting text-center" tabindex="0">No</th>
-                                        <th class="sorting text-center" tabindex="0">Nama Posisi</th>
-                                        <th class="sorting text-center" tabindex="0">Deskripsi</th>
-                                        <th class="sorting text-center" tabindex="0">Persyaratan</th>
-                                        <th class="sorting text-center" tabindex="0">Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($dataJob as $item)
-                                        <tr>
-                                            <td class="sorting text-center" tabindex="0">{{$loop->iteration}}</td>
-                                            <td class="text-center">{{$item->name}}</td>
-                                            <td class="text-center">{{$item->descript}}</td>
-                                            <td class="text-center">{{$item->required}}</td>
-                                            <td class="text-center">
-                                                <form action="/admin/job/{{$item->id}}" method="POST" style="display: inline; margin: auto 5px">
-                                                    @method('delete')
-                                                    @csrf
-                                                    <button class="btn btn-pink btn-icon btn-circle add-tooltip" data-toggle="tooltip" data-container="body" data-placement="top" data-original-title="Hapus Data">
-                                                        <i class="fa fa-trash icon-lg"></i>
-                                                    </button>
-                                                </form>
-                                            </td>
+                            <form action="{{ url('/admin/job/delete')}}" method="POST" style="display: inline">
+                                @method('delete')
+                                @csrf
+                                <button type="submit" name="submit" style="margin-top: -15px" class="btn btn-danger">Delete All Selected</button>
+                            </div>
+                            </div>
+                                <table id="masterdata-division"
+                                class="table table-striped table-bordered dataTable no-footer dtr-inline collapsed"
+                                role="grid" aria-describedby="demo-dt-basic_info" style="width: 100%;" width="100%"
+                                cellspacing="0">
+                                    <thead>
+                                        <tr role="row">
+                                            <th class="sorting text-center" tabindex="0" style="width: 5%">No</th>
+                                            <th class="sorting text-center" tabindex="0" style="width: 6%">All <input type="checkbox" id="master"></th>
+                                            <th class="sorting text-center" tabindex="0">Nama Posisi</th>
+                                            <th class="sorting text-center" tabindex="0">Deskripsi</th>
+                                            <th class="sorting text-center" tabindex="0">Persyaratan</th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($dataJob as $item)
+                                            <tr>
+                                                <td class="sorting text-center" tabindex="0">{{$loop->iteration}}</td>
+                                                <td class="text-center"><input type="checkbox" class="sub_chk" name="check[]" value="{{$item->id}}"></td>
+                                                <td class="text-center">{{$item->name}}</td>
+                                                <td class="text-center">{{$item->descript}}</td>
+                                                <td class="text-center">{{$item->required}}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </form>
+                            <div class="row">
+                                <div class="col-sm-5"></div>
+                                <div class="col-sm-2">
+                                    <ul class="pagination">
+                                        {{ $dataJob->links() }}
+                                    </ul>
+                                </div>
+                                <div class="col-sm-5"></div>
+                            </div>
                     @endif
                 </div>
             </div>
             <!--===================================================-->
             <!-- End Striped Table -->
     </div>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#master').on('click', function(e) {
+                if($(this).is(':checked',true)) {
+                    $(".sub_chk").prop('checked', true);  
+                }
+                else {  
+                    $(".sub_chk").prop('checked',false);  
+                }  
+            });
+        });
+    </script>
 @endsection

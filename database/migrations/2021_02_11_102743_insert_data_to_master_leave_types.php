2d4658599226345ee9cdca98073819908058bc5b
mesayusriana12 <mesayusriana12@gmail.com>
2021-02-11 10:39:15 +0700

    insert auto data, modal still bug

---
+++ database/migrations/2021_02_11_102743_insert_data_to_master_leave_types.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class InsertDataToMasterLeaveTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('master_leave_types', function (Blueprint $table) {
+            //
+        });
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('master_leave_types')->insert([
+            [
+                'id' => 1,
+                'name' => 'Cuti Tahunan',
+                'default_day' => 12
+            ],
+            [
+                'id' => 2,
+                'name' => 'Cuti Melahirkan',
+                'default_day' => 45
+            ],
+            [
+                'id' => 3,
+                'name' => 'Cuti Menikah',
+                'default_day' => 3
+            ]
+            ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('master_leave_types', function (Blueprint $table) {
+            //
+        });
+    }
+}

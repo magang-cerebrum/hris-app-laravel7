3b09e9d125ff4940092fd0d49309697875503ce4
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-02-15 14:44:56 +0700

    update create jadwal

---
+++ resources/views/masterData/schedule/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/templateAdmin')
+@section('title','Data Staff')
+@section('content-title','Data Staff / Tambah Jadwal Kerja')
+@section('content-subtitle','HRIS PT. Cerebrum Edukanesia Nusantara')
+@section('content')
+
+@endsection

4551bddeee5ff21394079be7adda9d58c1d4fc56
Yoga Prianjaya <yogaprianjaya@gmail.com>
2021-07-01 13:02:20 +0700

    update fitur presensi

--- app/MasterPresence.php
+++ app/MasterPresence.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MasterPresence extends Model
 {
-    protected $fillable = ['user_id','in_time','out_time','inaday_time','late_time','fine','presence_date','shift_name','shift_default_hour','status','file_in','check_chief'];
-    protected $nullable = ['in_time','out_time','inaday_time','late_time','late_time_rounded','shift_name','shift_default_hour'];
+    protected $fillable = ['user_id','in_time','out_time','inaday_time','late_time','fine','presence_date','shift_name','shift_default_hour','status','file_in','file_out','check_chief'];
+    protected $nullable = ['in_time','out_time','inaday_time','late_time','late_time_rounded','shift_name','shift_default_hour','file_in','file_out'];
 }
